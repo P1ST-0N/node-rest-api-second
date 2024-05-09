6144ea66506b1c4add7d831f6517a43b576c434f
P1ST-0N <luckylionia@gmail.com>
2024-05-09 18:03:20 +0300

    added hw03

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,11 +1,83 @@
-import contactsService from "../services/contactsServices.js";
+import Contact from "../models/contact.js";
 
-export const getAllContacts = (req, res) => {};
+import HttpError from "../helpers/HttpError.js";
 
-export const getOneContact = (req, res) => {};
+export const getAllContacts = async (req, res, next) => {
+  try {
+    const contacts = await Contact.find();
+    res.status(200).json(contacts);
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const deleteContact = (req, res) => {};
+export const getOneContact = async (req, res, next) => {
+  const { id } = req.params;
 
-export const createContact = (req, res) => {};
+  try {
+    const contact = await Contact.findById(id);
+    if (contact) {
+      res.status(200).json(contact);
+    } else {
+      throw HttpError(404, "Not found");
+    }
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const updateContact = (req, res) => {};
+export const createContact = async (req, res, next) => {
+  try {
+    const result = await Contact.create(req.body);
+
+    res.status(201).json(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteContact = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const deletedContact = await Contact.findByIdAndDelete(id);
+    if (deletedContact) {
+      res.status(200).json({ deletedContact, message: "Delete Success" });
+    } else {
+      throw HttpError(404, "Not found");
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateContact = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    const updatedContact = await Contact.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    if (updatedContact) {
+      return res
+        .status(200)
+        .json({ updatedContact, message: "Updated Success" });
+    } else {
+      throw HttpError(404, "Not found");
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateStatusContact = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await Contact.findByIdAndUpdate(id, req.body, { new: true });
+    if (!result) {
+      throw HttpError(404, "Not found");
+    }
+    res.status(200).json({ result, message: "Status contact updated Success" });
+  } catch (error) {
+    next(error);
+  }
+};
--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -1,9 +1,26 @@
 import Joi from "joi";
 
 export const createContactSchema = Joi.object({
-
-})
+  name: Joi.string().min(3).max(30).required(),
+  email: Joi.string()
+    .email({
+      minDomainSegments: 2,
+      tlds: { allow: ["com", "net"] },
+    })
+    .required(),
+  phone: Joi.string().min(3).max(15).required(),
+});
 
 export const updateContactSchema = Joi.object({
+  name: Joi.string().min(3).max(30),
+  email: Joi.string().email({
+    minDomainSegments: 2,
+    tlds: { allow: ["com", "net"] },
+  }),
+  phone: Joi.string().min(3).max(13),
+  favorite: Joi.boolean(),
+});
 
-})
+export const updateFavoriteSchema = Joi.object({
+  favorite: Joi.boolean().required(),
+});
