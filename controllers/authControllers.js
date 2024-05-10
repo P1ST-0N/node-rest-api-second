3c197862d93115ce2edefd71777eaac3d9fdf3b6
P1ST-0N <luckylionia@gmail.com>
2024-05-10 15:56:27 +0300

    added subscriptionUpdate func

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -60,3 +60,39 @@ export const logout = async (req, res, next) => {
     next(error);
   }
 };
+
+export const getCurrent = async (req, res, next) => {
+  try {
+    const { email, subscription } = req.user;
+    res.json({ email, subscription });
+  } catch {
+    next(error);
+  }
+};
+
+export const subscriptionUpdate = async (req, res, next) => {
+  try {
+    const { subscription: newValueSub } = req.body;
+    const { email, _id } = req.user;
+
+    switch (newValueSub) {
+      case "starter":
+      case "business":
+      case "pro":
+        const updatedUser = await User.findOneAndUpdate(
+          { _id },
+          { subscription: newValueSub },
+          { new: true }
+        );
+        res.json({
+          email,
+          subscription: updatedUser.subscription,
+        });
+        break;
+      default:
+        res.status(400).json({ message: "This subscription does not exist" });
+    }
+  } catch (error) {
+    next(error);
+  }
+};
--- routes/authRouter.js
+++ routes/authRouter.js
@@ -4,8 +4,9 @@ import { authSchema } from "../schemas/authSchema.js";
 import {
   register,
   login,
-  //   getCurrent,
+  getCurrent,
   logout,
+  subscriptionUpdate,
 } from "../controllers/authControllers.js";
 
 import authenticate from "../helpers/authenticate.js";
@@ -14,7 +15,10 @@ const authRouter = express.Router();
 
 authRouter.post("/register", validateBody(authSchema), register);
 authRouter.post("/login", validateBody(authSchema), login);
-// authRouter.get("/current", authenticate, getCurrent);
 authRouter.post("/logout", authenticate, logout);
+
+authRouter.get("/current", authenticate, getCurrent);
+
+authRouter.patch("/", authenticate, subscriptionUpdate);
 
 export default authRouter;
