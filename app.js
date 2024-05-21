45cdcfbe9d3a704c0204e553b2448d1c341f14b9
P1ST-0N <luckylionia@gmail.com>
2024-05-21 11:49:59 +0300

    fix models/user

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ app.use(morgan("dev"));
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
 
 app.use("/api/contacts", contactsRouter);
 app.use("/api/users", authRouter);
