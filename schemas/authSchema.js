d3319df5da2dee06c8c2d8f45055819b9a40f57b
P1ST-0N <luckylionia@gmail.com>
2024-05-24 15:28:20 +0300

    Update authSchema.js

--- schemas/authSchema.js
+++ schemas/authSchema.js
@@ -2,5 +2,5 @@ import Joi from "joi";
 
 export const authSchema = Joi.object({
   password: Joi.string().min(3).required(),
-  email: Joi.string().email().required(),
+  email: Joi.string().email().trim().lowercase().required(),
 });
